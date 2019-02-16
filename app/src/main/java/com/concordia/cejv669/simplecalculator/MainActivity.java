0c35853cb78d1a94be2c6b61a896d91ded005981
zhenqianfeng <ferretfeng@yahoo.com>
2019-02-16 00:10:55 -0500

    Initial commit: Layout only

---
+++ app/src/main/java/com/concordia/cejv669/simplecalculator/MainActivity.java
@@ -0,0 +1,13 @@
+package com.concordia.cejv669.simplecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
